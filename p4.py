0abd1084856f76c08f5f3ac861bf93ef0d1e2550
blackdragon0465-coder <blackdragon0465@gmail.com>
2025-11-27 15:42:07 +0530

    Update p4.py

--- p4.py
+++ p4.py
@@ -1,16 +1,27 @@
 import sys
-if len(sys.argv) < 2:
-    print("Usage: python even_odd_count.py <num1> <num2> <num3> ...")
-    sys.exit(1)
 
-numbers = [int(n) for n in sys.argv[1:]]
+if len(sys.argv) > 1:
+    script_name = sys.argv[0]
+    numbers = sys.argv[1:]
+    print("User provided input values:")
+else:
+    script_name = sys.argv[0]
+    numbers = ["10", "21", "32", "45", "50"]
+    print("No input given — using default values:")
+
+numbers = [int(n) for n in numbers]
+
 even_count = 0
 odd_count = 0
-for num in numbers:
-    if num % 2 == 0:
+
+for n in numbers:
+    if n % 2 == 0:
         even_count += 1
     else:
         odd_count += 1
 
+print("\n--- Even and Odd Count ---")
+print("Script Name:", script_name)
+print("Numbers:", numbers)
 print("Even Numbers Count:", even_count)
 print("Odd Numbers Count:", odd_count)
